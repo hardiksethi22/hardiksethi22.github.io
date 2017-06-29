192ee1c1d84330d0512d2c3c9458d61e99ff148b
Hardik Sethi <hardiksethi893@gmail.com>
2017-06-30 02:33:02 +0530

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,21 +1,13 @@
 <?php
-
-$name = mysqli_real_escape_string($con, $_POST['contactName']);
-$mes = mysqli_real_escape_string($con, $_POST['contactEmail']);
-$sub = mysqli_real_escape_string($con, $_POST['contactSubject']);
-$message = mysqli_real_escape_string($con, $_POST['contactMessage']);
-$to="hardiksethi893@gmail.com";
-/*$qu = mysqli_query($con,"SELECT * FROM users_cl where email='$mes'");
-            if(mysqli_num_rows($qu) > 0){
-
-                while($row = mysqli_fetch_assoc($qu)){
-           $temp_id= $row['id'];
-           $temp_id_check=$temp_id*2879;
-$ms= "Please visit canceraidsociety.org/newpassword.php?token=$temp_id_check&mail=$mes to change your account password";
-*/
-                    mail($to,$sub,$message,'FROM: hardiksethi893@gmail.com');
-                      
-   
-   header("location:index.html");
- 
-?>
+require 'PHPMailerAutoload.php';
+$mail = new PHPMailer;
+$mail->setFrom('manusethi123@gmail.com', 'hardik');
+$mail->addAddress('hardiksethi893@gmail.com', 'My Friend');
+$mail->Subject  = 'First PHPMailer Message';
+$mail->Body     = 'Hi! This is my first e-mail sent through PHPMailer.';
+if(!$mail->send()) {
+  echo 'Message was not sent.';
+  echo 'Mailer error: ' . $mail->ErrorInfo;
+} else {
+  echo 'Message has been sent.';
+}
