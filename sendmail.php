f0d60bb62298ab420c0e990dfdc8b8d6af760fc1
Hardik Sethi <hardiksethi893@gmail.com>
2017-06-30 02:45:23 +0530

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,13 +1,2 @@
 <?php
-require 'PHPMailerAutoload.php';
-$mail = new PHPMailer;
-$mail->setFrom('manusethi123@gmail.com', 'hardik');
-$mail->addAddress('hardiksethi893@gmail.com', 'My Friend');
-$mail->Subject  = 'First PHPMailer Message';
-$mail->Body     = 'Hi! This is my first e-mail sent through PHPMailer.';
-if(!$mail->send()) {
-  echo 'Message was not sent.';
-  echo 'Mailer error: ' . $mail->ErrorInfo;
-} else {
-  echo 'Message has been sent.';
-}
+echo "asdsa";
